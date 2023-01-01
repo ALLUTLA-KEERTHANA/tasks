cf7b4c3c8d3dbaada218eb199e5455dd5600616c
ALLUTLA-KEERTHANA <119609181+ALLUTLA-KEERTHANA@users.noreply.github.com>
2023-01-01 18:21:00 +0530

    Add files via upload

---
+++ naturalnumber.java
@@ -0,0 +1,13 @@
+package welcome;
+
+public class naturalnumber {
+	public static void main(String[] args) {
+
+		System.out.println("The First 10 Natural Numbers are");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		
+		}
+	}
+}
+
